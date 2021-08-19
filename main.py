da5e856806db21db39530c3cdc3faeeb1435b05a
Vinicius Mendes Barbosa <viniciusmbarbosa2014@gmail.com>
2021-08-19 18:59:58 -0300

    Implementação da Função "get_abstract"

--- main.py
+++ main.py
@@ -27,9 +27,13 @@ def get_title(site, lan_quant = 1):
             title = site.find('h'+str(cont), attrs={'class': 'article-title'})
             list_title.append(title.text)
         return (list_title)
+    
+def get_abstract(site, lan_quant=1):
+    if lan_quant == 1:
+        section = site.find('article', attrs={'class': 'col-md-10 col-md-offset-2 col-sm-12 col-sm-offset-0', 'id':'articleText'})
+        abstract = section.find('p').text
+        return abstract
 
 link = test_connection(input('Digite o link do artigo'))
 site = capture_html(link)
-title = get_title(site, 2)
-print(title)
-print(title[0])
+section = site.find('article', attrs={'class': 'col-md-10 col-md-offset-2 col-sm-12 col-sm-offset-0', 'id':'articleText'})
