dff2ec4b98a329e961e9a4b40bf12f6c5375bb56
Vinicius Mendes Barbosa <viniciusmbarbosa2014@gmail.com>
2021-08-21 14:50:56 -0300

    Inicio das funçoes de formatação

---
+++ capture.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+
+def test_connection(link):
+    while True:
+        try:
+            response = requests.get(link)
+        except:
+            link = input('Link fora do ar ou invalido, tente novamente: ')
+            continue
+        else:
+            return (link)
+        
+def capture_html(link):
+    content = requests.get(link).content
+    site = BeautifulSoup(content, 'html.parser')
+    return(site)
+
+def get_title(site, lan_quant = 1):
+    if lan_quant == 1:
+        title = site.find('h1', attrs={'class': 'article-title'})
+        return (title.text)
+    else:
+        list_title = []
+        cont = 0
+        while cont < lan_quant:
+            cont += 1
+            title = site.find('h'+str(cont), attrs={'class': 'article-title'})
+            list_title.append(title.text)
+        return (list_title)
+    
+def get_abstract(site):
+        section = site.find('article', attrs={'class': 'col-md-10 col-md-offset-2 col-sm-12 col-sm-offset-0', 'id':'articleText'})
+        abstract = section.find('p')
+        return abstract.text
+
+def get_keyword(site):
+    section = site.find('article', attrs={'class': 'col-md-10 col-md-offset-2 col-sm-12 col-sm-offset-0', 'id':'articleText'})
+    keyword = section.find('br').next_element
+    return keyword
+
+def get_author(site):
+    section = site.find('div', attrs={'class': 'contribGroup'})
+    authors = section.find_all('a')
+    author = []
+    range_authors = len((authors))
+    for i in range(range_authors):
+        if i == range_authors-1:
+            pass
+        elif i % 2 == 0 or i == 0 :
+             author.append(authors[i].text)
+    if len(author) == 2:
+        author.pop()
+    return author
--- main.py
+++ main.py
@@ -1,55 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
+import capture
 
-def test_connection(link):
-    while True:
-        try:
-            response = requests.get(link)
-        except:
-            link = input('Link fora do ar ou invalido, tente novamente: ')
-            continue
-        else:
-            return (link)
-        
-def capture_html(link):
-    content = requests.get(link).content
-    site = BeautifulSoup(content, 'html.parser')
-    return(site)
+link = capture.test_connection(input('Digite qual o link do resumo? '))
+site = capture.capture_html(link)
+title = capture.get_title(site)
+authors = capture.get_author(site)
+abstract = capture.get_abstract(site)
+keywords = capture.get_keyword(site)
+print(type(title))
+print(type(authors[0]))
 
-def get_title(site, lan_quant = 1):
-    if lan_quant == 1:
-        title = site.find('h1', attrs={'class': 'article-title'})
-        return (title.text)
-    else:
-        list_title = []
-        cont = 0
-        while cont < lan_quant:
-            cont += 1
-            title = site.find('h'+str(cont), attrs={'class': 'article-title'})
-            list_title.append(title.text)
-        return (list_title)
-    
-def get_abstract(site):
-        section = site.find('article', attrs={'class': 'col-md-10 col-md-offset-2 col-sm-12 col-sm-offset-0', 'id':'articleText'})
-        abstract = section.find('p')
-        return abstract.text
 
-def get_keyword(site):
-    section = site.find('article', attrs={'class': 'col-md-10 col-md-offset-2 col-sm-12 col-sm-offset-0', 'id':'articleText'})
-    keyword = section.find('br').next_element
-    return keyword
+def txt_facebook(title, authors, caminho):
+    try:
+        open(caminho, 'r+') 
+    except FileNotFoundError:
+        open(caminho, 'w+')    
+    facebook = open(caminho, 'a')
+    facebook.write(title + ' - ' + authors[0])
 
-def get_author(site):
-    section = site.find('div', attrs={'class': 'contribGroup'})
-    authors = section.find_all('a')
-    author = []
-    range_authors = len((authors))
-    for i in range(range_authors):
-        if i == range_authors-1:
-            pass
-        elif i % 2 == 0 or i == 0 :
-             author.append(authors[i].text)
-    return author
-    
-link = test_connection(input('Digite o link do artigo: '))
-site = capture_html(link)
+caminho = '/home/vinicius/Área de Trabalho/aaaaaa'
+txt_facebook(title, authors, caminho)
